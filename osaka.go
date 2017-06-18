18e5fca9ac1fd9a1ed245341ef101ed9a66b9726
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-06-18 23:26:59 +0900

    fix replace

--- osaka.go
+++ osaka.go
@@ -2,9 +2,26 @@ package osaka
 
 import "strings"
 
+func replace(src, s, r string) string {
+	dest := ""
+	i := 0
+	i0 := 0
+	for i != -1 {
+		i = strings.Index(src[i0:], s)
+		if i != -1 {
+			dest = dest + src[i0:i] + r
+			i += len(s)
+			i0 = i
+		} else {
+			dest = dest + src[i0:len(src)]
+		}
+	}
+	return dest
+}
+
 func Encode(s string) string {
 	for k, v := range tbl {
-		s = strings.Replace(s, k, v, -1)
+		s = replace(s, k, v)
 	}
 	return s
 
